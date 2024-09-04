6dc900f6553138496753e27cbe1d0b86c5ccad27
senhorvance <senhor.vance@proton.me>
2024-09-04 14:17:48 +0000

    deployment stage

--- interactive_quiz/settings.py
+++ interactive_quiz/settings.py
@@ -75,11 +75,11 @@ WSGI_APPLICATION = "interactive_quiz.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse("postgresql://interactive_quiz_user:DSgmvzAglIOay6KfRxY4Oke7BMfoBG1e@dpg-crc6falds78s73978vc0-a.oregon-postgres.render.com/interactive_quiz"),
 }
 
 
