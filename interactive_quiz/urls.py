b933cf67f062fae08577704588c22a5ac321b01d
Malaya Manas <malayamanas@gmail.com>
2025-04-06 19:56:47 +0400

    Update urls.py

--- interactive_quiz/urls.py
+++ interactive_quiz/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
