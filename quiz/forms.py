b108e08299482e7fbfebb389c87d3a1d325dd0bc
Malaya Manas <malayamanas@gmail.com>
2025-04-06 18:26:42 +0400

    Update forms.py

--- quiz/forms.py
+++ quiz/forms.py
@@ -2,6 +2,12 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Question
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['text', 'is_true', 'image']  # Include the new image field
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
