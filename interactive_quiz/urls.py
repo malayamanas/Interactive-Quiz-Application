e96384ee3619347550eda951524c9822e9d2d81c
Malaya Manas <malayamanas@gmail.com>
2025-04-06 19:55:42 +0400

    Update urls.py

--- interactive_quiz/urls.py
+++ interactive_quiz/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
